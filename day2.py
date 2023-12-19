358cb2a1233a34ed7ed4e469bc8d7f129b5c2d2b
deanmajaya <deanmajaya@gmail.com>
2023-12-19 11:51:25 +0200

    dealing with python data types

---
+++ day2.py
@@ -0,0 +1,38 @@
+# Data types
+
+# String
+
+# Integer
+# writing long numbers, _ replaces the commas
+123_456_789
+
+num_char = len(input('What is your name?'))
+
+# returns <class 'int'>, use to investigate data types you are working with 
+print(type(num_char))
+
+# converts to string
+str(num_char)
+
+# converts to float
+print(70 + float('100.58'))
+
+# convert to int
+print(70 + int(100.58))
+
+# mathematical calculations
+6 + 3
+6 - 3
+6 * 3
+
+# returns a float
+6 / 3
+
+# returns int
+6 // 3
+
+6 ** 3
+
+# PEDMASLR
+print(3 * ((3 + 3) / 3) - 3)
+
